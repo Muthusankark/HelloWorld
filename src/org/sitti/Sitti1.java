d7007969b9b38650f5df7a9e9a71d8b3e8bbea1d
Muthusankark <useemuthu@gmail.com>
2021-09-07 22:24:03 +0530

    busines functionality

--- src/org/sitti/Sitti1.java
+++ src/org/sitti/Sitti1.java
@@ -3,10 +3,18 @@ package org.sitti;
 public class Sitti1 {
 	
 	private void sittiName() {
+		System.out.println("SittiSitti");
 
 	}
 	private void sittiProcesser() {
+		System.out.println("2TB");
 
+	}
+	public static void main(String[] args) {
+		Sitti1 si = new Sitti1();
+		si.sittiName();
+		si.sittiProcesser();
+		
 	}
 
 }
