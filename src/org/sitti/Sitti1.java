617da0d0c1227edb4556adb3e89ee289d1838cd2
Muthusankark <useemuthu@gmail.com>
2021-09-07 22:16:14 +0530

    business Functionality

--- src/org/sitti/Sitti1.java
+++ src/org/sitti/Sitti1.java
@@ -3,10 +3,19 @@ package org.sitti;
 public class Sitti1 {
 	
 	private void sittiName() {
+		System.out.println("sitti");
 
 	}
 	private void sittiProcesser() {
+		System.out.println("1TB");
 
+	}
+	public static void main(String[] args) {
+		
+		Sitti1 si = new Sitti1();
+		si.sittiName();
+		si.sittiProcesser();
+		
 	}
 
 }
